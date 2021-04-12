73c6b5118f1eac45cbec291bb3f0ffdeca821a2d
Kyu Chul Kim <31753713+goodcheer@users.noreply.github.com>
2021-04-12 23:11:02 +0900

    add recursion method

--- week1/subsets_28ms_14mb.py
+++ week1/subsets_28ms_14mb.py
@@ -15,3 +15,26 @@ class Solution:
             k += 1
 
         return res + [nums]
+    
+    
+class Solution2:
+    """recursion"""
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first = 0, curr = []):
+            # if the combination is done
+            if len(curr) == k:  
+                output.append(curr[:])
+                return
+            for i in range(first, n):
+                # add nums[i] into the current combination
+                curr.append(nums[i])
+                # use next integers to complete the combination
+                backtrack(i + 1, curr)
+                # backtrack
+                curr.pop()
+        
+        output = []
+        n = len(nums)
+        for k in range(n + 1):
+            backtrack()
+        return output
