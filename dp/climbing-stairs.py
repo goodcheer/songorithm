b0c632b949f2b1b3b62d29d30e9c79e78e09f0a2
Kyu Chul Kim <31753713+goodcheer@users.noreply.github.com>
2021-06-10 08:49:15 +0900

    Create climbing-stairs.py

---
+++ dp/climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+  """
+  https://leetcode.com/problems/climbing-stairs/solution/
+  """
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+            # print(dp)
+        return dp[n]
